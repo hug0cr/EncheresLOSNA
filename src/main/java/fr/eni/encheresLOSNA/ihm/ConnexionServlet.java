174e2c6d581c3805a80ef2216b7f54b7b46edc64
adverot <ad.verot@gmail.com>
2021-06-01 16:42:41 +0200

    Suppression du constructeur de la servlet Connexion

--- src/main/java/fr/eni/encheresLOSNA/ihm/ConnexionServlet.java
+++ src/main/java/fr/eni/encheresLOSNA/ihm/ConnexionServlet.java
@@ -15,14 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 public class ConnexionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ConnexionServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
