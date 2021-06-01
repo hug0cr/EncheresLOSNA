487619fdadfdc70d862475f8fb709ac403d616de
hug0cr <pro.hugocamacho@gmail.com>
2021-06-01 09:04:14 +0200

    Ajout classe utilisateur

---
+++ src/main/java/fr/eni/encheresLOSNA/bo/Utilisateur.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package fr.eni.encheresLOSNA.bo;
+
+/**
+ * @author hug0cr
+ * @version EncheresLOSNA - V1.0
+ * @date 1 juin 2021 - 08:54:35
+ */
+public class Utilisateur {
+	private int noUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private String codePostal;
+	private String ville;
+	private String motDePasse;
+	private int credit;
+	private boolean administrateur;
+	
+}
