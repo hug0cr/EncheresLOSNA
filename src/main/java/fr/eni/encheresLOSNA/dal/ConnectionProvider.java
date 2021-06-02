c7dfe5d37748803eed18eb03d131a0a424c676c6
hug0cr <pro.hugocamacho@gmail.com>
2021-06-02 10:18:21 +0200

    Ajout de la classe ConnectionProvider et du fichier context.xml

---
+++ src/main/java/fr/eni/encheresLOSNA/dal/ConnectionProvider.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package fr.eni.encheresLOSNA.dal;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ * @author hug0cr
+ * @version EncheresLOSNA - V1.0
+ * @date 2 juin 2021 - 10:14:04
+ */
+public abstract class ConnectionProvider {
+	private static DataSource dataSource;
+    
+    /**
+     * Au chargement de la classe, la DataSource est recherchée dans l'arbre JNDI
+     */
+    static
+    {
+        Context context;
+        try {
+            context = new InitialContext();
+            ConnectionProvider.dataSource = (DataSource)context.lookup("java:comp/env/jdbc/pool_cnx");
+        } catch (NamingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Impossible d'accéder à la base de données");
+        }
+    }
+    
+    /**
+     * Cette méthode retourne une connection opérationnelle issue du pool de connexion
+     * vers la base de données. 
+     * @return
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws SQLException
+    {
+        return ConnectionProvider.dataSource.getConnection();
+    }
+}
