c0b47eab9dfc350390b720dd5a1725b34cfe3365
adverot <ad.verot@gmail.com>
2021-06-04 16:19:40 +0200

    Correction Filtrage UTF-8

--- src/main/java/fr/eni/encheresLOSNA/ihm/FiltrageServlet.java
+++ src/main/java/fr/eni/encheresLOSNA/ihm/FiltrageServlet.java
@@ -32,6 +32,7 @@ public class FiltrageServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		Integer categorie = Integer.parseInt(request.getParameter("categorie"));
 		String motCle = (String) request.getParameter("motCle");
 		ArticleVenduManager articleMgr = ArticleVenduManager.getInstance();
